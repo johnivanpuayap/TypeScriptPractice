27060f40973b11357fd36fc2252a0ff73ccd03a0
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-08 21:37:52 +0800

    Arrays

---
+++ 02Basics/myArrays.ts
@@ -0,0 +1,25 @@
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+
+// another way to declare an array
+// they work exactly the same
+const heroPower: Array<number> = [];
+
+type User = {
+    name: string;
+    isActive: boolean;
+}
+
+const allUsers: Array<User> = [];
+
+superHeroes.push('Spider-man');
+heroPower.push(5);
+
+allUsers.push({name: "", isActive: true})
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    [],
+];
+
+const defaults: ReadonlyArray<number> = [];
