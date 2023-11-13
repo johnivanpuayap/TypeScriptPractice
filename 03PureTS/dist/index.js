64e44bd3a01279d3243c2076c9f4625d3bd471c2
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-14 00:07:46 +0800

    Getters and Setters, and Access Modifiers

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -1,11 +1,54 @@
 "use strict";
 // Creating a Class
+// class User {
+//     public email: string;
+//     // default is public
+//     private name: string;
+//     readonly city: string = "Cebu City";
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 class User {
-    constructor(email, name) {
-        this.city = "Cebu City";
+    // shorter way of creating the constructor
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        //Protected is like private but it can be accessed by the child class
+        this._courseCount = 1;
+        this.city = "Cebu City";
+    }
+    // private methods
+    deleteToken() {
+        console.log("Token Deleted");
+    }
+    //getter
+    get getAppleEmail() {
+        return "apple${this.email}";
+    }
+    get getCourseCount() {
+        return this._courseCount;
+    }
+    // setter
+    // return type should be blank and not even void
+    set setCourseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than one");
+        }
+        this._courseCount = courseNum;
+    }
+}
+// Inheritance
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 5;
     }
 }
 // Creating an Object
-const ivan = new User("johnivanpuayap@gmail.com", "Ivan Puayap");
+const ivan = new User("johnivanpuayap@gmail.com", "Ivan Puayap", "1");
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,14 +1,61 @@
 // Creating a Class
-class User {
+// class User {
+//     public email: string;
+//     // default is public
+//     private name: string;
+//     readonly city: string = "Cebu City";
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 
-    email: string;
-    name: string;
+class User {
+    //Protected is like private but it can be accessed by the child class
+    protected _courseCount = 1
+
     readonly city: string = "Cebu City";
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
+
+    // shorter way of creating the constructor
+    constructor(
+        public email: string, 
+        public name: string,
+        private userId: string,
+    ) {}
+
+    // private methods
+    private deleteToken() {
+        console.log("Token Deleted");
+    }
+
+    //getter
+    get getAppleEmail(): string{
+        return "apple${this.email}"
+    }
+
+    get getCourseCount(): number{
+        return this._courseCount;
+    }
+
+    // setter
+    // return type should be blank and not even void
+    set setCourseCount(courseNum: number){
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than one")
+        }
+        this._courseCount = courseNum;
+    }
+}
+
+// Inheritance
+
+class SubUser extends User {
+    isFamily: boolean = true;
+
+    changeCourseCount() {
+        this._courseCount = 5;
     }
 }
 
 // Creating an Object
-const ivan = new User("johnivanpuayap@gmail.com", "Ivan Puayap");
+const ivan = new User("johnivanpuayap@gmail.com", "Ivan Puayap", "1");
