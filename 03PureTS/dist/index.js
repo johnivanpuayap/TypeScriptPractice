6eb527fea5aaeb21b07b8075d5c5bc262c2be823
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-13 23:49:01 +0800

    Classes

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log('Hello TypeScript!');
-console.log('Try');
+// Creating a Class
+class User {
+    constructor(email, name) {
+        this.city = "Cebu City";
+        this.email = email;
+        this.name = name;
+    }
+}
+// Creating an Object
+const ivan = new User("johnivanpuayap@gmail.com", "Ivan Puayap");
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,2 +1,14 @@
-console.log('Hello TypeScript!')
-console.log('Try')
+// Creating a Class
+class User {
+
+    email: string;
+    name: string;
+    readonly city: string = "Cebu City";
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+// Creating an Object
+const ivan = new User("johnivanpuayap@gmail.com", "Ivan Puayap");
